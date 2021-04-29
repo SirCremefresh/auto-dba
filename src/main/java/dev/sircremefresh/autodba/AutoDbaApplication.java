c30993a69ff69a2fd139854e5ea6664fd52b4f20
SirCremefresh <donato.wolfisberg@gmail.com>
2021-04-29 20:21:54 +0200

    Initial

---
+++ src/main/java/dev/sircremefresh/autodba/AutoDbaApplication.java
@@ -0,0 +1,13 @@
+package dev.sircremefresh.autodba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoDbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sircremefresh/autodba/AutoDbaApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.sircremefresh.autodba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import io.fabric8.kubernetes.client.KubernetesClient;
+
+@SpringBootTest
+class AutoDbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
