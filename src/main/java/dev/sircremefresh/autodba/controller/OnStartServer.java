2c12e77bd25de6ec4f2749276184d084fd0bfc74
SirCremefresh <donato.wolfisberg@gmail.com>
2021-05-03 21:22:01 +0200

    R

--- src/main/java/dev/sircremefresh/autodba/controller/OnStartServer.java
+++ src/main/java/dev/sircremefresh/autodba/controller/OnStartServer.java
@@ -2,6 +2,8 @@ package dev.sircremefresh.autodba.controller;
 
 import dev.sircremefresh.autodba.controller.crd.Database;
 import dev.sircremefresh.autodba.controller.crd.DatabaseList;
+import dev.sircremefresh.autodba.controller.crd.DatabaseSpec;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,48 +25,40 @@ public class OnStartServer implements ApplicationListener<ContextRefreshedEvent>
 
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-		System.out.println("asldkfjaölskdfö. datasourceUrl: " + datasourceUrl);
-
-//		client.customResources(Database.class).withName("").edit((Database database) -> {
-//			database.setStatus(
-//					DatabaseStatus.builder()
-//							.lastTransitionTime(LocalDateTime.now())
-//							.status("True")
-//							.type("ad")
-//							.build()
-//			);
-//		});
+		System.out.println("datasourceUrl: " + datasourceUrl);
 
 		val databaseClient = client.customResources(Database.class, DatabaseList.class);
 
-		val databaseResource = databaseClient.inAnyNamespace().list();
-		System.out.println(databaseResource.getItems());
 
-//		items.forEach(database -> {
-//			database.setStatus(
-//					DatabaseStatus.builder()
-//							.lastTransitionTime(LocalDateTime.now())
-//							.status("True")
-//							.type("ad")
-//							.build()
-//			);
-//
-//			System.out.println("Found resource. " + database + " ---------- " +
-//					"name: " + database.getMetadata().getName() +
-//					", version: " + database.getMetadata().getResourceVersion());
-//		});
-//
-//		List<Pod> list = client.pods().list().getItems();
-//
-//		list.forEach(pod -> {
-//			System.out.println("Found resource. " +
-//					"name: " + pod.getMetadata().getName() +
-//					", version: " + pod.getMetadata().getResourceVersion());
-//		});
-//
-//		client.pods().watch(new Watcher<>() {
+		Database d = new Database();
+		d.setMetadata(
+				new ObjectMetaBuilder()
+						.withName("aaaaa")
+						.build()
+		);
+		d.setSpec(DatabaseSpec.builder()
+				.databaseName("asdfasd")
+				.secretName("asdfasdfasd").build());
+
+		databaseClient.inNamespace("auto-dba-dev").create(d);
+
+//		databaseClient.watch(new Watcher<>() {
 //			@Override
-//			public void eventReceived(Action action, Pod resource) {
+//			public void eventReceived(Action action, Database resource) {
+////				resource.setStatus(
+////						DatabaseStatus.builder()
+////								.type("True")
+////								.status("True")
+////								.lastTransitionTime(LocalDateTime.now())
+////								.build()
+////				);
+////				resource.setSpec(
+////						resource.getSpec().toBuilder()
+////								.databaseName("laskjdflaskd")
+////								.build()
+////				);
+//				resource.getSpec().setDatabaseName("alskdfj");
+//				databaseClient.replace(resource);
 //				System.out.println("Found resource. " +
 //						"name: " + resource.getMetadata().getName() +
 //						", version: " + resource.getMetadata().getResourceVersion());
@@ -78,5 +72,8 @@ public class OnStartServer implements ApplicationListener<ContextRefreshedEvent>
 //				}
 //			}
 //		});
+
+//		val databaseResource = databaseClient.inAnyNamespace().list();
+//		System.out.println(databaseResource.getItems());
 	}
 }
