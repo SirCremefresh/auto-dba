80201e9cb6c2c5a3b2061c80ebf8874ffe3dd1c9
SirCremefresh <donato.wolfisberg@gmail.com>
2021-05-07 18:53:16 +0200

    new builder

--- src/main/java/dev/sircremefresh/autodba/controller/database/crd/Database.java
+++ src/main/java/dev/sircremefresh/autodba/controller/database/crd/Database.java
@@ -5,9 +5,11 @@ import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
 import io.fabric8.kubernetes.model.annotation.Kind;
 import io.fabric8.kubernetes.model.annotation.Version;
+import io.sundr.builder.annotations.Buildable;
 
 @Version("v1alpha1")
 @Group("autodba.sircremefresh.dev")
 @Kind("Database")
+@Buildable
 public class Database extends CustomResource<DatabaseSpec, DatabaseStatus> implements Namespaced {
 }
