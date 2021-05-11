95971c922f9f7999b1accb2cb940caeb01b4f1af
SirCremefresh <donato.wolfisberg@gmail.com>
2021-05-11 18:34:58 +0200

    R

--- src/main/java/dev/sircremefresh/autodba/controller/OnStartServer.java
+++ src/main/java/dev/sircremefresh/autodba/controller/OnStartServer.java
@@ -8,8 +8,8 @@ import io.fabric8.kubernetes.client.informers.ResourceEventHandler;
 import io.fabric8.kubernetes.client.informers.SharedIndexInformer;
 import io.fabric8.kubernetes.client.informers.SharedInformerFactory;
 import io.fabric8.kubernetes.internal.KubernetesDeserializer;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -19,9 +19,6 @@ public class OnStartServer implements ApplicationListener<ContextRefreshedEvent>
 	private final KubernetesClient client;
 	private final DatabaseReconciler databaseReconciler;
 
-	@Value("${spring.datasource.url}")
-	private String datasourceUrl;
-
 	@Autowired
 	public OnStartServer(KubernetesClient client, DatabaseReconciler databaseReconciler) {
 		this.client = client;
@@ -29,8 +26,7 @@ public class OnStartServer implements ApplicationListener<ContextRefreshedEvent>
 	}
 
 	@Override
-	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-		System.out.println("datasourceUrl: " + datasourceUrl);
+	public void onApplicationEvent(@NonNull ContextRefreshedEvent contextRefreshedEvent) {
 		KubernetesDeserializer.registerCustomKind("v1alpha1", "Database", Database.class);
 
 		SharedInformerFactory sharedInformerFactory = client.informers();
@@ -57,56 +53,5 @@ public class OnStartServer implements ApplicationListener<ContextRefreshedEvent>
 		});
 
 		databaseInformer.run();
-
-//		Database d = new Database();
-//		d.setMetadata(
-//				new ObjectMetaBuilder()
-//						.withName("aaaaa")
-//						.build()
-//		);
-//		d.setSpec(DatabaseSpec.builder()
-//				.databaseName("asdfasd")
-//				.secretName("asdfasdfasd").build());
-//
-//		databaseClient.inNamespace("auto-dba-dev").create(d);
-
-//		databaseClient.inAnyNamespace().watch(new Watcher<>() {
-//			@Override
-//			public void eventReceived(Action action, Database resource) {
-//				databaseClient.inNamespace(resource.getMetadata().getNamespace()).withName(resource.getMetadata().getName()).edit(database -> {
-//					database.getSpec().setDatabaseName("alsdf");
-//					return database;
-//				});
-//				resource.setStatus(
-//						DatabaseStatus.builder()
-//								.type("True")
-//								.status("True")
-//								.lastTransitionTime(LocalDateTime.now())
-//								.build()
-//				);
-//				resource.setSpec(
-//						resource.getSpec().toBuilder()
-//								.databaseName("laskjdflaskd")
-//								.build()
-//				);
-//				resource.getMetadata().setName("asdfasdddddd");
-//				resource.getSpec().setDatabaseName("alskdfj");
-//				databaseClient.replace(resource);
-//				System.out.println("Found resource. " +
-//						"name: " + resource.getMetadata().getName() +
-//						", version: " + resource.getMetadata().getResourceVersion());
-//			}
-//
-//			@Override
-//			public void onClose(WatcherException cause) {
-//				if (cause != null) {
-//					cause.printStackTrace();
-//					System.exit(-1);
-//				}
-//			}
-//		});
-
-//		val databaseResource = databaseClient.inAnyNamespace().list();
-//		System.out.println(databaseResource.getItems());
 	}
 }
