097eec41253522ebf87359bddd78a83b0d2dc19e
SirCremefresh <donato.wolfisberg@gmail.com>
2021-05-11 18:32:56 +0200

    not working

--- src/main/java/dev/sircremefresh/autodba/controller/database/crd/Database.java
+++ src/main/java/dev/sircremefresh/autodba/controller/database/crd/Database.java
@@ -4,12 +4,14 @@ import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
 import io.fabric8.kubernetes.model.annotation.Kind;
+import io.fabric8.kubernetes.model.annotation.Plural;
 import io.fabric8.kubernetes.model.annotation.Version;
 import io.sundr.builder.annotations.Buildable;
 
 @Version("v1alpha1")
 @Group("autodba.sircremefresh.dev")
 @Kind("Database")
+@Plural("Databases")
 @Buildable
 public class Database extends CustomResource<DatabaseSpec, DatabaseStatus> implements Namespaced {
 }
--- src/test/java/dev/sircremefresh/autodba/AutoDbaApplicationTests.java
+++ src/test/java/dev/sircremefresh/autodba/AutoDbaApplicationTests.java
@@ -3,12 +3,12 @@ package dev.sircremefresh.autodba;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AutoDbaApplicationTests {
-
-	@Test
-	void contextLoads() {
-
-	}
-
-}
+//@SpringBootTest
+//class AutoDbaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//
+//	}
+//
+//}
