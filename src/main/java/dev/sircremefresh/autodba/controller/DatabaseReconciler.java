a1c1dc19828e35bf2346355e10f1577d413f2bf5
SirCremefresh <donato.wolfisberg@gmail.com>
2021-05-15 16:45:24 +0200

    added database key logic

--- src/main/java/dev/sircremefresh/autodba/controller/DatabaseReconciler.java
+++ src/main/java/dev/sircremefresh/autodba/controller/DatabaseReconciler.java
@@ -10,6 +10,7 @@ import io.fabric8.kubernetes.client.dsl.Resource;
 import io.fabric8.kubernetes.client.informers.cache.Cache;
 import lombok.NonNull;
 import lombok.val;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.postgresql.Driver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,7 +21,6 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Optional;
@@ -32,6 +32,9 @@ public class DatabaseReconciler {
 	private static final Logger logger = LoggerFactory.getLogger(DatabaseReconciler.class.getName());
 	private static final String USERNAME_KEY = "username";
 	private static final String PASSWORD_KEY = "password";
+	private static final int MAX_DATABASE_KEY_LENGTH = 63;
+	private static final int DATABASE_KEY_HASH_LENGTH = 11;
+	private static final int DATABASE_KEY_IDENTIFIER_LENGTH = 50;
 
 	final JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
@@ -50,18 +53,9 @@ public class DatabaseReconciler {
 			throw new FatalBeanException(msg, err);
 		}
 
-//		val conn = DriverManager.getConnection("");
-
 		databaseClient = client.customResources(Database.class, DatabaseList.class);
 	}
 
-	public DataSource createDataSource(@NonNull String url, @NonNull String username, @NonNull String password) {
-		DriverManagerDataSource ds = new DriverManagerDataSource();
-		ds.setUrl(url);
-		ds.setUsername(username);
-		ds.setPassword(password);
-		return ds;
-	}
 
 	private Optional<Connection> getConnection(@NonNull ClusterDatabaseServer databaseServer, @NonNull Secret secret) {
 		val data = secret.getStringData();
@@ -77,8 +71,6 @@ public class DatabaseReconciler {
 			return Optional.empty();
 		}
 	}
-
-//	private loadSecretFor
 
 	public void reconcile(Database database, @Nullable ClusterDatabaseServer databaseServer, @Nullable Secret secret) {
 		logger.info("Reconcile database {}", database.getMetadata().getName());
@@ -100,7 +92,7 @@ public class DatabaseReconciler {
 								logger.error("Error occurred in database connection for database {} in databaseServer {}", Cache.metaNamespaceKeyFunc(database), Cache.metaNamespaceKeyFunc(databaseServer));
 							}
 						},
-						() -> logger.error("Could not connect to databaseServer {} for database {}", Cache.metaNamespaceKeyFunc(databaseServer), Cache.metaNamespaceKeyFunc(database))
+						() -> logger.error("Could not get connection to databaseServer {} for database {}", Cache.metaNamespaceKeyFunc(databaseServer), Cache.metaNamespaceKeyFunc(database))
 				);
 
 //		System.out.println(jdbcTemplate.getQueryTimeout());
@@ -134,8 +126,24 @@ public class DatabaseReconciler {
 //		}
 	}
 
-	private void handleDatabase(@NonNull Database database, @NonNull Connection connection) {
 
+	private void handleDatabase(@NonNull Database database, @NonNull Connection connection) {
+		val databaseKey = generateDatabaseKey(database.getMetadata().getNamespace(), database.getMetadata().getName());
+	}
+
+	private String generateDatabaseKey(@NonNull String namespace, @NonNull String name) {
+		if (namespace.length() + name.length() + 1 <= MAX_DATABASE_KEY_LENGTH) {
+			return namespace + "-" + name;
+		}
+		val hash = createDatabaseKeyHash(namespace, name);
+		val identifierFieldLength = DATABASE_KEY_IDENTIFIER_LENGTH / 2;
+		if (name.length() <= identifierFieldLength) {
+			return namespace.substring(0, DATABASE_KEY_IDENTIFIER_LENGTH - name.length()) + "-" + name + "-" + hash;
+		}
+		if (namespace.length() <= identifierFieldLength) {
+			return namespace + "-" + name.substring(0, DATABASE_KEY_IDENTIFIER_LENGTH - namespace.length()) + "-" + hash;
+		}
+		return namespace.substring(0, identifierFieldLength) + "-" + name.substring(0, identifierFieldLength) + "-" + hash;
 	}
 
 	private void createUser(String user, String password) {
@@ -149,8 +157,8 @@ public class DatabaseReconciler {
 
 	private boolean doesUserExist(String user) {
 		val res = jdbcTemplate.query(
-				"SELECT count(*) as count FROM pg_database WHERE datname=?",
-				(rs, rowNum) -> rs.getLong("count") > 0,
+				"SELECT count(*) > 0 as exists FROM pg_database WHERE datname=?",
+				(rs, rowNum) -> rs.getBoolean("exists"),
 				user);
 		return res.get(0);
 	}
@@ -165,5 +173,9 @@ public class DatabaseReconciler {
 			out.append(chars.charAt(index));
 		}
 		return out.toString();
+	}
+
+	private String createDatabaseKeyHash(@NonNull String namespace, @NonNull String name) {
+		return DigestUtils.sha1Hex(namespace + "-" + name).substring(0, DATABASE_KEY_HASH_LENGTH);
 	}
 }
