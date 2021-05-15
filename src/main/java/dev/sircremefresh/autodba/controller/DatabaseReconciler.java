00daad76a182faecbf8d2793f4be7bb007059ed6
SirCremefresh <donato.wolfisberg@gmail.com>
2021-05-15 19:21:31 +0200

    fix decoding secret and replace - with _ on database keys

--- src/main/java/dev/sircremefresh/autodba/controller/DatabaseReconciler.java
+++ src/main/java/dev/sircremefresh/autodba/controller/DatabaseReconciler.java
@@ -12,7 +12,6 @@ import io.fabric8.kubernetes.client.informers.cache.Cache;
 import lombok.NonNull;
 import lombok.val;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.postgresql.Driver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.FatalBeanException;
@@ -24,9 +23,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Random;
+import java.util.*;
 import java.util.regex.Pattern;
 
 @Repository
@@ -49,10 +46,11 @@ public class DatabaseReconciler {
 		this.client = client;
 
 		try {
-			if (!Driver.isRegistered()) {
-				Driver.register();
-			}
-		} catch (SQLException err) {
+//			if (!Driver.isRegistered()) {
+//				Driver.register();
+//			}
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException err) {
 			val msg = "Could not register postgresql driver.";
 			logger.error(msg, err);
 			throw new FatalBeanException(msg, err);
@@ -61,6 +59,9 @@ public class DatabaseReconciler {
 		databaseClient = client.customResources(Database.class, DatabaseList.class);
 	}
 
+	private String b64Decode(String encoded) {
+		return new String(Base64.getDecoder().decode(encoded)).trim();
+	}
 
 	private Optional<JdbcTemplate> getConnection(@NonNull ClusterDatabaseServer databaseServer, @NonNull Secret secret) {
 		val data = secret.getData();
@@ -68,8 +69,10 @@ public class DatabaseReconciler {
 			logger.warn("Secret {} for databaseServer {} does not contain username or password", Cache.metaNamespaceKeyFunc(secret), Cache.metaNamespaceKeyFunc(databaseServer));
 			return Optional.empty();
 		}
-		String jdbcUrl = "jdbc:postgresql://" + databaseServer.getSpec().getHost() + ":" + databaseServer.getSpec().getPort();
-		val manager = new DriverManagerDataSource(jdbcUrl, data.get(USERNAME_KEY), data.get(PASSWORD_KEY));
+		val username = b64Decode(data.get(USERNAME_KEY));
+		val password = b64Decode(data.get(PASSWORD_KEY));
+		String jdbcUrl = "jdbc:postgresql://" + databaseServer.getSpec().getHost() + ":" + databaseServer.getSpec().getPort() + "/";
+		val manager = new DriverManagerDataSource(jdbcUrl, username, password);
 		try {
 			Objects.requireNonNull(manager.getConnection());
 		} catch (SQLException err) {
@@ -146,24 +149,27 @@ public class DatabaseReconciler {
 	}
 
 	private String generateDatabaseKey(@NonNull String namespace, @NonNull String name) {
+		namespace = namespace.replace('-','_').toLowerCase(Locale.ROOT);
+		name = name.replace('-','_').toLowerCase(Locale.ROOT);
+
 		if (namespace.length() + name.length() + 1 <= MAX_DATABASE_KEY_LENGTH) {
-			return namespace + "-" + name;
+			return namespace + "_" + name;
 		}
 		val hash = createDatabaseKeyHash(namespace, name);
 		val identifierFieldLength = DATABASE_KEY_IDENTIFIER_LENGTH / 2;
 		if (name.length() <= identifierFieldLength) {
-			return namespace.substring(0, DATABASE_KEY_IDENTIFIER_LENGTH - name.length()) + "-" + name + "-" + hash;
+			return namespace.substring(0, DATABASE_KEY_IDENTIFIER_LENGTH - name.length()) + "_" + name + "_" + hash;
 		}
 		if (namespace.length() <= identifierFieldLength) {
-			return namespace + "-" + name.substring(0, DATABASE_KEY_IDENTIFIER_LENGTH - namespace.length()) + "-" + hash;
+			return namespace + "_" + name.substring(0, DATABASE_KEY_IDENTIFIER_LENGTH - namespace.length()) + "_" + hash;
 		}
-		return namespace.substring(0, identifierFieldLength) + "-" + name.substring(0, identifierFieldLength) + "-" + hash;
+		return namespace.substring(0, identifierFieldLength) + "_" + name.substring(0, identifierFieldLength) + "_" + hash;
 	}
 
 	private void createDatabaseAndUser(@NonNull String user, @NonNull String password, @NonNull JdbcTemplate jdbcTemplate) {
-		val reg = "^[a-z0-9_]{3,59}$";
+		val reg = "^[a-z0-9_]{3,63}$";
 		if (!Pattern.matches(reg, user) || !Pattern.matches(reg, password)) {
-			throw new IllegalStateException("Username or Password has illegal characters" + user);
+			throw new IllegalStateException("Username or Password has illegal characters " + user);
 		}
 
 		jdbcTemplate.execute("create user " + user + " with encrypted password '" + password + "';");
@@ -178,7 +184,7 @@ public class DatabaseReconciler {
 	}
 
 	private String genPassword() {
-		String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+		String chars = "abcdefghijklmnopqrstuvwxyz1234567890";
 		StringBuilder out = new StringBuilder();
 		Random rnd = new Random();
 		while (out.length() < 32) {
@@ -189,6 +195,6 @@ public class DatabaseReconciler {
 	}
 
 	private String createDatabaseKeyHash(@NonNull String namespace, @NonNull String name) {
-		return DigestUtils.sha1Hex(namespace + "-" + name).substring(0, DATABASE_KEY_HASH_LENGTH);
+		return DigestUtils.sha1Hex(namespace + "_" + name).substring(0, DATABASE_KEY_HASH_LENGTH);
 	}
 }
